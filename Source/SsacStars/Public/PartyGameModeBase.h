3e439c9786cd1190c8a76b4538de0d0fa1b12114
choi06151 <78068768+choi06151@users.noreply.github.com>
2024-02-23 17:46:50 +0900

    GoodJobBros!

--- Source/SsacStars/Public/PartyGameModeBase.h
+++ Source/SsacStars/Public/PartyGameModeBase.h
@@ -14,4 +14,32 @@ class SSACSTARS_API APartyGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+
+	UENUM(BlueprintType)
+		enum class EPlayerState : uint8
+	{
+		WaitTurn	UMETA(DisplayName = "상대턴 대기"),
+		StartTurn	UMETA(DisplayName = "내턴시작"),
+		SelectBehavior	UMETA(DisplayName = "주사위or아이템or맵"),
+		ThrowDice	UMETA(DisplayName = "주사위던지기"),
+		Interaction		UMETA(DisplayName = "발판상호작용"),
+		PlayAnimation		UMETA(DisplayName = "여러애니메이션 "),
+		StartMinigame		UMETA(DisplayName = "미니게임 시작")
+	};
+
+
+	UENUM(BlueprintType)
+		enum class ESpaceState : uint8
+	{
+		Blue	UMETA(DisplayName = "효과없음"),
+		Red		UMETA(DisplayName = "코인-3"),
+		Item	UMETA(DisplayName = "아이템얻기"),
+		Trap	UMETA(DisplayName = "함정발판"),
+		Warp		UMETA(DisplayName = "워프발판"),
+		Star		UMETA(DisplayName = "별얻기"),
+		2SideLoad		UMETA(DisplayName = "갈림길")
+	};
+
+
+
 };
