74eb2e2513c184a52d97aecabce0a01ba472fe8f
jyunseon81 <jyunseon81@gmail.com>
2024-03-13 18:57:42 +0900

    Add Kirby AnimInstance

---
+++ Source/SsacStars/Public/SsacGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SsacGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SSACSTARS_API USsacGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
